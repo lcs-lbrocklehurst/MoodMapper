448581634075acd2f25925b0cde0dd0be20b239d
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-04-13 11:50:47 -0400

    added a basic static interface

--- MoodMapper/ContentView.swift
+++ MoodMapper/ContentView.swift
@@ -9,18 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        //MARK: computed properties
+        
+        
+        //MARK: stored properties
+        NavigationView {
+            
+            VStack {
+                
+                HStack {
+                    
+                    TextField("Enter your current mood", text: Binding.constant(""))
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("ADD")
+                            .font(.caption)
+                    })
+                    
+                }
+                .padding(20)
+                
+                List {
+                  
+                        
+                        Text("Shocked")
+                        
+                    
+                    
+                    Text("Chillin")
+                    Text("Embarrased")
+                }
+                
+                
+                
+            
+                
+            }
+            .navigationTitle("Mood Mapper")
         }
-        .padding()
+        
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
