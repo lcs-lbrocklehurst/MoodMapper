a7668168b98676bb63338ddb6b770ba41e7d6699
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-04-13 11:12:42 -0400

    Initial Commit

---
+++ MoodMapper/MoodMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMapperApp.swift
+//  MoodMapper
+//
+//  Created by Lewis Brocklehurst on 2023-04-13.
+//
+
+import SwiftUI
+
+@main
+struct MoodMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
